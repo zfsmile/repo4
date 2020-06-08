c63e1a8ad6f9f50e2f2dce0330a1b5fa8cef71b1
zfsmile <wei6896@163.com>
2020-06-08 13:26:54 +0800

    idea java

---
+++ day49git_test_project/src/top/zfshuma/git/firstClass.java
@@ -0,0 +1,13 @@
+package top.zfshuma.git;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello！");
+        System.out.println("hello！");
+        System.out.println("hello！");
+        System.out.println("hello！");
+        System.out.println("hello！");
+        System.out.println("hello！");
+        System.out.println("hello！");
+    }
+}
